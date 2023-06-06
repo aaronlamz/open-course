74e9951da340ddf06a9a9251d082ba4b2e872888
aaronlamz <aaronlamz2022@gmail.com>
2023-06-06 21:12:46 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,14 @@ echo pnpm install
 pnpm install
 echo pnpm run build
 pnpm build:nodejs
+
+cd packages/nodejs/docs/.vitepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+git init
+git add .
+git commit -m 'deploy site page'
+git status
+git push -f git@github.com:aaronlamz/open-ebook.git main:gh-pages
+
