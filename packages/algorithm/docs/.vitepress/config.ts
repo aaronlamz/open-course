60af751c6ce849be2d6a9d62dc7db64deeec0666
aaronlamz <aaronlamz2022@gmail.com>
2023-06-15 13:47:42 +0800

    add algorithm

---
+++ packages/algorithm/docs/.vitepress/config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "数据结构与算法学习指南",
+  description: "数据结构与算法学习指南",
+  base: '/open-ebook/algorithm/',
+  outDir: './.vitepress/dist/algorithm',
+  head: [['link', { rel: 'icon', href: '/open-ebook/algorithm/favicon.ico' }]],
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: '/hero.png',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '指南', link: '/setup/introduction.md' }
+    ],
+
+    sidebar: [
+      {
+        text: '基础入门',
+        items: [
+          { text: 'Node.js 简介', link: '/setup/introduction.md' },
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/aaronlamz/open-ebook' }
+    ]
+  }
+})
---
+++ packages/algorithm/docs/index.md
@@ -0,0 +1,30 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "数据结构与算法学习指南"
+  text: "结构化学习，逐步提升"
+  tagline: "从零开始学习数据结构与算法"
+
+  image:
+    src: /hero.png
+    alt: Hero image
+  actions:
+    - theme: brand
+      text: 开始学习
+      link: /markdown-examples
+    - theme: alt
+      text: 查看Github
+      link: https://github.com/aaronlamz/open-ebook
+
+features:
+
+  - title: 深入浅出
+    details: 从零开始，深入浅出，逐步提升
+  - title: 构建体系
+    details: 告别碎片化学习，构建自己的知识体系
+  - title: 开发实践
+    details: 通过实践，巩固所学知识
+---
+
