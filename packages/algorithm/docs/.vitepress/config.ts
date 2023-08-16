4714fa62027e70766e9da806e74e1c866afe6854
aaronlamz <aaronlamz2022@gmail.com>
2023-08-17 00:03:07 +0800

    update --docs

--- packages/algorithm/docs/.vitepress/config.ts
+++ packages/algorithm/docs/.vitepress/config.ts
@@ -2,26 +2,33 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "数据结构与算法学习指南",
-  description: "数据结构与算法学习指南",
+  title: "数据结构与算法之美",
+  description: "数据结构与算法之美",
   base: '/open-course/algorithm/',
   outDir: './.vitepress/dist/algorithm',
   head: [['link', { rel: 'icon', href: '/open-course/algorithm/favicon.ico' }]],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
-    logo: '/hero.png',
+    logo: '/logo.svg',
     nav: [
       { text: '首页', link: '/' },
-      { text: '指南', link: '/setup/introduction.md' }
+      { text: '指南', link: '/guide/introduction.md' },
+      { text: 'GitHub', link: 'https://github.com/aaronlamz/open-course' },
     ],
 
     sidebar: [
       {
         text: '基础入门',
         items: [
-          { text: 'Node.js 简介', link: '/setup/introduction.md' },
+          { text: '简介', link: '/guide/introduction.md' },
         ]
-      }
+      },
+      {
+        text: '算法实践',
+        items: [
+          { text: '剖析Redis常用数据类型对应的数据结构', link: '/practice/redis.md' },
+        ]
+      },
     ],
 
     socialLinks: [
--- packages/algorithm/docs/index.md
+++ packages/algorithm/docs/index.md
@@ -3,17 +3,17 @@
 layout: home
 
 hero:
-  name: "数据结构与算法学习"
-  text: "结构化学习，逐步提升"
+  name: "数据结构与算法之美"
+  text: "系统化学习，逐步提升"
   tagline: "从零开始学习数据结构与算法"
 
   image:
-    src: /hero.png
+    src: /logo.svg
     alt: Hero image
   actions:
     - theme: brand
       text: 开始学习
-      link: /markdown-examples
+      link: /guide/introduction.md
     - theme: alt
       text: 查看Github
       link: https://github.com/aaronlamz/open-course
@@ -24,7 +24,7 @@ features:
     details: 从零开始，深入浅出，逐步提升
   - title: 构建体系
     details: 告别碎片化学习，构建自己的知识体系
-  - title: 开发实践
-    details: 通过实践，巩固所学知识
+  - title: 算法实践
+    details: 算法实践，提升编程能力
 ---
 
