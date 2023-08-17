e47c2288ce295703e8045997956b0937380cc91f
aaronlamz <aaronlamz2022@gmail.com>
2023-08-17 21:05:26 +0800

    update

--- packages/qiankun/docs/.vitepress/config.ts
+++ packages/qiankun/docs/.vitepress/config.ts
@@ -30,13 +30,26 @@ export default defineConfig({
         ]
       },
       {
-        text: '后台管理系统实践',
+        text: '微前端应用实践-后台管理系统',
         items: [
           { text: '选择路由模式', link: '/admin/router.md' },
+          { text: '构建基座应用', link: '/admin/base.md' },
+          { text: '构建子应用', link: '/admin/sub.md' },
+          { text: '构建子应用-React', link: '/admin/sub-react.md' },
+          { text: '构建子应用-Vue', link: '/admin/sub-vue.md' },
+          { text: '构建子应用-Angular', link: '/admin/sub-angular.md' },
+          { text: '父子应用间通信', link: '/admin/communication.md' },
+          { text: '样式隔离方案', link: '/admin/style.md' },
+          { text: 'Tab页签 KeepAlive 方案', link: '/admin/keep-alive.md' },
         ]
       },
       {
-        text: '源码解析',
+        text: '常见问题&解决方案',
+        items: [
+        ]
+      },
+      {
+        text: '源码解析篇',
         items: [
           { text: 'qiankun 源码解析', link: '/source/qiankun.md' },
         ]
@@ -53,3 +66,4 @@ export default defineConfig({
     ]
   }
 })
+
